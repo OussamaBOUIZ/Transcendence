170e6a47d1bd5d24098ea19b44cc8b7d8d4fbb73
Issam Jmari <ijmari@e1r3p4.1337.ma>
2023-07-06 18:49:40 +0100

    full stack expirement

--- dist/databases/muted_users.entity.d.ts
+++
@@ -1,3 +0,0 @@
-import { BaseEntity } from "typeorm";
-export declare class Muted_users extends BaseEntity {
-}
--- dist/databases/stats.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { BaseEntity } from "typeorm";
-import { Achievement } from "./achievement.entity";
-export declare class Stats extends BaseEntity {
-    id: number;
-    achievements: Achievement[];
-    wins: number;
-    losses: number;
-    ladder_level: number;
-}
