38b764208791181bb121ac739ab2c67de67f9511
ael-mous <teqaniabde@gmail.com>
2023-07-13 13:03:31 +0100

    fix dependencies problems

--- app/backend/src/chat/userchat.gateway.ts
+++ app/backend/src/chat/userchat.gateway.ts
@@ -50,13 +50,13 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
         this.logger = new Logger(ChatGateway.name);
     }
 
-    // @SubscribeMessage('message')
-    // async loadMessages(socket: Socket, data: { Receiver: ReceiverDto }) {
-    //     let user = await this.chatGatewayService.getUserById(data.Receiver.userId)
-    //     if (!user)
-    //         this.logger.log('todo handle if not exist!!')
-    //
-    // }
+    @SubscribeMessage('message')
+    async loadMessages(socket: Socket, data: { Receiver: ReceiverDto }) {
+        let user = await this.chatGatewayService.getUserById(data.Receiver.userId)
+        if (!user)
+            this.logger.log('todo handle if not exist!!')
+
+    }
 
     @SubscribeMessage('SendMessage')
     async sendMessage(socket: Socket, data: MessageDto) {
@@ -64,28 +64,12 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
         if (!receiver)
             console.log('TODO : handle if the receiver not exist')
         this.logger.log({receiver})
-        this.logger.log(socket.data.user.email)
-        // find the sender by email
-
-        const db_user = await this.userRepository.findOneBy({email: socket.data.user.email})
-        this.logger.log({db_user})
-        console.log(db_user.id)
-        await this.chatGatewayService.saveMessage(data, receiver, db_user.id)
+        this.logger.log({sender: socket.data.user})
+        console.log(socket.data.user.id)
+        await this.chatGatewayService.saveMessage(data, receiver, socket.data.user.id)
         this.server.to(receiver.socketId).emit("message", data.message)
     }
 
-    @SubscribeMessage('loadMessages')
-    async allMessages(socket: Socket, data: MessageDto)
-    {
-        const receiver = await this.chatGatewayService.getUserById(data.user.userId)
-        if (!receiver)
-            console.log('todo: handle if the receiver not exist')
-
-        const db_user = await this.userRepository.findOneBy({email: socket.data.user.email})
-
-        await this.chatGatewayService.loadMessage(db_user, receiver.id)
-
-    }
 
     afterInit(client: Socket) {
         client.use(SocketAuthMiddleware(this.chatGatewayService) as any)
@@ -94,20 +78,10 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
 
     async handleConnection(client: Socket) {
         const {authorization} = client.handshake.headers;
-        let db_user: User
+
         const user = this.chatGatewayService.getUser(authorization)
-        console.log(user)
-        db_user = await this.userRepository.findOneBy({email: user.email})
-        if (db_user) {
-            db_user.socketId = client.id
-        }
-        else
-        {
-            db_user = new User()
-            db_user.email = user.email
-            db_user.socketId = client.id;
-        }
-        await this.userRepository.save(db_user)
+        user.socketId = client.id
+        await this.userRepository.save(user)
     }
 
 
--- app/backend/src/chat/userchat.module.ts
+++ app/backend/src/chat/userchat.module.ts
@@ -1,28 +1,28 @@
-// import {Module} from "@nestjs/common";
-// import {ChatGateway} from "./userchat.gateway";
-// import {ChatGatewayService} from "./userchat.service";
-// import {TypeOrmModule} from "@nestjs/typeorm";
-// import {User} from "../databases/user.entity";
-// import {JwtService} from "@nestjs/jwt";
-// import {WsGuard} from "../auth/socketGuard/wsGuard";
-// import {User_chat} from "../databases/userchat.entity";
-// import {Message} from "../databases/message.entity";
+import {Module} from "@nestjs/common";
+import {ChatGateway} from "./userchat.gateway";
+import {ChatGatewayService} from "./userchat.service";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {User} from "../databases/user.entity";
+import {JwtService} from "@nestjs/jwt";
+import {WsGuard} from "../auth/socketGuard/wsGuard";
+import {User_chat} from "../databases/userchat.entity";
+import {Message} from "../databases/message.entity";
 
 
-// @Module({
-//     imports: [
-//         TypeOrmModule.forFeature([User, User_chat, Message]),
-//         // JwtModule.registerAsync({
-//         // useFactory: async (configService: ConfigService) => ({
-//         //     global: true,
-//         //     secret: configService.get('JWT_SECRET'),
-//         // }),
-//         // inject: [ConfigService],
-//         // })
-//     ],
-//     controllers: [],
-//     providers: [ChatGatewayService, ChatGateway, JwtService, WsGuard]
-// })
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([User, User_chat, Message]),
+        // JwtModule.registerAsync({
+        // useFactory: async (configService: ConfigService) => ({
+        //     global: true,
+        //     secret: configService.get('JWT_SECRET'),
+        // }),
+        // inject: [ConfigService],
+        // })
+    ],
+    controllers: [],
+    providers: [ChatGatewayService, ChatGateway, JwtService, WsGuard]
+})
 
-// export class ChatGatewayModule {
-// }
+export class ChatGatewayModule {
+}
