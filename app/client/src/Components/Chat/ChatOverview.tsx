2b4f5cf39a6778926ff85ce1253c0a5854ef86b8
OussamaBOUIZ <oussamabouizgarne861@gmail.com>
2023-07-14 21:38:25 +0100

    Chat Overview Init case styled for ref screen reso

--- app/client/src/Components/Chat/ChatOverview.tsx
+++ app/client/src/Components/Chat/ChatOverview.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
 export default function ChatOverview () {
-    return (<div className="chat_overview"></div>);
+    return (<div className="chat_overview">
+        <p className="guide_text">
+        As channels come alive
+        this box will soon be filled
+        with fellow members.
+        Engage in captivating
+        conversations, share
+        experiences, and foster 
+        connections with
+        like-minded individuals
+        </p>
+    </div>);
 }
