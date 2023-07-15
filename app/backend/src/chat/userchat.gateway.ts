796bac51f247740da5a1e95a8749ff26e91ac8de
abdelhak4 <teqaniabde@gmail.com>
2023-07-15 08:17:06 +0100

    save

--- app/backend/src/chat/userchat.gateway.ts
+++ app/backend/src/chat/userchat.gateway.ts
@@ -7,8 +7,8 @@ import {
     WebSocketServer
 } from "@nestjs/websockets";
 import {Server, Socket} from 'socket.io';
-import {ChatGatewayService} from "./userchat.service";
 import {JwtService} from "@nestjs/jwt";
+import {ChatGatewayService} from "./userchat.service"
 import {ConfigService} from "@nestjs/config";
 import {InjectRepository} from "@nestjs/typeorm";
 import {Repository} from "typeorm";
