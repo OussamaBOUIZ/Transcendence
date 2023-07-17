f01dc790294156551504acf2918f7850238d3cb8
issamjmari <issamjmari098@gmail.com>
2023-07-17 15:20:59 +0100

    ubuntu email confirmation not working

--- app/backend/src/auth/MailService/mailer.service.ts
+++ app/backend/src/auth/MailService/mailer.service.ts
@@ -8,9 +8,9 @@ export class MailTemplate {
 
   async sendEmail(toUser: string) {
     const from: string = 'issamjmari098@gmail.com';
-    const to: string = 'slash.dq.99@gmail.com';
+    const to: string = toUser;
     const subject: string = 'Email confirmation';
-    const htmlfile = fs.readFileSync('/Users/oouazize/Desktop/Transcendence/app/backend/src/auth/htmlSources/file.html', 'utf-8');
+    const htmlfile = fs.readFileSync('/home/ijmari/Desktop/Transcendence/app/backend/src/auth/htmlSources/file.html', 'utf-8');
     console.log(`${to}`);
     const v = await this.mailerService.sendMail({
       from, 
