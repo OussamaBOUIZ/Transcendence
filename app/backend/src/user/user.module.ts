0425b79d866de969179cbe9115a665f54be63cb6
issamjmari <issamjmari098@gmail.com>
2023-07-22 19:50:38 +0100

    mute,ban, kick and many features coming

--- app/backend/src/user/user.module.ts
+++ app/backend/src/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { User } from 'src/databases/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, JwtService],
+  imports: [TypeOrmModule.forFeature([User])]
 })
 export class UserModule {}
