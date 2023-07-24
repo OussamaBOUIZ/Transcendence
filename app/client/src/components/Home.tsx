fffc3d0acecf903eb2c72040cfa99b5a96db3252
Ouazize <oouazize@student.1337.ma>
2023-07-24 15:34:47 +0100

    refactor: remove some unexpected pushed files

--- app/client/src/components/Home.tsx
+++ app/client/src/components/Home.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from "react"
 import {useState} from "react"
 import axios from "axios"
+import "./home.scss";
 
 export default function Home() {
 
@@ -12,24 +13,30 @@ export default function Home() {
         username: ""
     })
 
-    useEffect(() => {
-        const getUserData = async () => {
-            try {
-                const response = await axios.get("/api/auth/getuser");
-                setUserData(response.data);
-            }
-            catch (error) {
-                console.log(error)
-            }
+    // useEffect(() => {
+    //     const getUserData = async () => {
+    //         try {
+    //             const response = await axios.get("/api/auth/getuser");
+    //             setUserData(response.data);
+    //         }
+    //         catch (error) {
+    //             console.log(error)
+    //         }
 
-        }
-        getUserData()
-    }, [])
+    //     }
+    //     getUserData()
+    // }, [])
 
     return (
-        <div className="welcome">
-            <h1>Hi {UserData.username} with id {UserData.id} and name {UserData.firstname} </h1>
-            <h3 className="hd">Welcome to your Home</h3>
+        <div className="grid">
+            {/* <h1>Hi {UserData.username} with id {UserData.id} and name {UserData.firstname} </h1>
+            <h3 className="hd">Welcome to your Home</h3> */}
+            <div className="item item1"></div>
+            <div className="item item2"></div>
+            <div className="item item3"></div>
+            <div className="item item4"></div>
+            <div className="item item5"></div>
+            <div className="item item6"></div>
         </div>
     )
 }
