e467812d0542fe44ea0b331023a77f52038eec10
OussamaBOUIZ <oussamabouizgarne861@gmail.com>
2023-07-30 19:23:21 +0100

    Refactor Chat

--- app/client/src/Components/Chat/ChatInput.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function ChatInput () {
-    return (<form className="chat_input">
-        <input type="text" placeholder='Type something'/>
-        <input type="submit" value="Send" />
-    </form>);
-}
--- app/client/src/Components/Chat/ChatMain.tsx
+++ app/client/src/Components/Chat/ChatMain.tsx
@@ -4,13 +4,22 @@ import ChatHeader from './ChatHeader';
 import ChatInput from './ChatInput';
 import ChatWindow from './ChatWindow';
 
+
 export default function ChatMain () {
+    
 
     return (
         <div className="chat_main">
              <ChatHeader />
-             <ChatWindow />
-            <ChatInput />
+             <section className="chat_window">
+            <div className="message_bubble">
+                Hello how're you doing
+            </div>
+        </section>
+             <form className="chat_input">
+                <input type="text" placeholder='Type something'/>
+                <input type="submit" value="Send" />
+            </form>
         </div>
     );
 }
