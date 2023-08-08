8b74909d4c7ab7ea194bd8878ad46218e100662a
ael-mous <teqaniabde@gmail.com>
2023-08-08 10:48:04 +0100

    still fixing bug in upload avatar

--- app/backend/src/user/user.controller.ts
+++ app/backend/src/user/user.controller.ts
@@ -1,291 +1,301 @@
 
 import {
-    Controller,
-    Header,
-    Headers,
-    Get,
-    Delete,
-    Param,
-    ParseIntPipe,
-    UseGuards,
-    StreamableFile,
-    UnauthorizedException,
-    UseInterceptors,
-    Post, Req, Res, HttpStatus, 
-    UploadedFile, Body, Patch, HttpCode, Query,
-    HttpException, ParseFilePipe, FileTypeValidator,
-    MaxFileSizeValidator,
+	Controller,
+	Header,
+	Headers,
+	Get,
+	Delete,
+	Param,
+	ParseIntPipe,
+	UseGuards,
+	StreamableFile,
+	UnauthorizedException,
+	UseInterceptors,
+	Post, Req, Res, HttpStatus,
+	UploadedFile, Body, Patch, HttpCode, Query,
+	HttpException, ParseFilePipe, FileTypeValidator,
+	MaxFileSizeValidator,
+	NotFoundException,
+	Put,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UserService } from './user.service';
-import {raw, Request, Response} from 'express'
+import { raw, Request, Response } from 'express'
 import { createReadStream, promises as fsPromises } from 'fs';
 import * as path from 'path';
-import {JwtGuard} from "../auth/jwt/jwtGuard";
+import { JwtGuard } from "../auth/jwt/jwtGuard";
 import { JwtService } from '@nestjs/jwt';
-import {Match_history} from "../databases/match_history.entity";
+import { Match_history } from "../databases/match_history.entity";
 import { BlockedTokenlistService } from 'src/databases/BlockedTokenList/BlockedTokenList.service';
 import { StatsDto } from './dto/stats-dto';
 import { GameHistoryDto } from './game-history-dto/game-history-dto';
 import { searchDto } from './game-history-dto/search-dto';
-import {diskStorage} from 'multer'
+import { diskStorage } from 'multer'
 import { Observable, of } from 'rxjs';
 import { isNumber } from 'class-validator';
 import { extname } from 'path';
+import { access } from 'fs/promises';
 
-enum FileValidationErrors {
-    UNSUPPORTED_FILE_TYPE
-}
 
-const multerConfig =   () => ({
+const DirUpload = './uploads/usersImage/'
+
+const multerConfig = () => ({
 	storage: diskStorage({
-		destination: './uploads/usersImage',
+		destination: DirUpload,
 		filename: async (req: any, file: any, cb: any) => {
-            const supportedExt = ['.png', '.jpeg', ['.jpg'] ]
-            if (isNaN(parseInt(req.params['userId'], 10)))
-                return cb(new HttpException('userId Must be a number', HttpStatus.BAD_REQUEST), false)
+			const supportedExt = ['.png', '.jpeg', ['.jpg']]
+			if (isNaN(parseInt(req.params['userId'], 10)))
+				return cb(new HttpException('userId Must be a number', HttpStatus.BAD_REQUEST), false)
 
-            console.log(file)
-            if (!supportedExt.includes(extname(file.originalname)))
-               return cb(new HttpException(`Unsupported file type ${file.originalname.ext}`, HttpStatus.BAD_REQUEST), false)
-			console.log('jjjjjsjjsjjsj')
-            const extention = path.parse(file.originalname).ext
+			console.log(file)
+			if (!supportedExt.includes(extname(file.originalname)))
+				return cb(new HttpException(`Unsupported file type ${file.originalname.ext}`, HttpStatus.BAD_REQUEST), false)
+			const extention = path.parse(file.originalname).ext
 			const filename = req.params['userId'] + extention
-            try {
-                await fsPromises.access(filename)
-                cb(new HttpException(`Wrong Http Method`, HttpStatus.METHOD_NOT_ALLOWED))
-            }
-            catch (e) {
-                cb(null, filename)
-            }
-        } 
+			try {
+				await fsPromises.access(DirUpload + filename)
+				console.log(filename)
+				cb(new HttpException(`Wrong Http Method`, HttpStatus.METHOD_NOT_ALLOWED))
+			}
+			catch (e) {
+				console.log(e)
+				cb(null, filename)
+			}
+		}
 	})
 })
 
 @Controller('user')
 @UseGuards(JwtGuard)
 export class UserController {
-    constructor(
+	constructor(
 		private readonly userService: UserService,
 		private readonly jwt: JwtService,
 		private readonly BlockedTokenService: BlockedTokenlistService
-	)  {
-    }
+	) {
+	}
 
-	@Post('/:userId/uploadf')
+	@Post('/:userId/upload')
 	@UseInterceptors(FileInterceptor('image', multerConfig()))
-	uploadImage (
+	// @HttpCode(HttpStatus.CREATED)
+	async uploadImage(
 		@Param('userId', ParseIntPipe) id: number,
 		@UploadedFile(new ParseFilePipe({
 			fileIsRequired: true,
-            validators: [
-                new FileTypeValidator({fileType: '.(png|jpeg|jpg)'}),
-                new MaxFileSizeValidator({maxSize: 1024}),
-            ]
-        })) image : Express.Multer.File
-	) : Observable<Object>
-	 {
-			console.log(image)
-			return of({imagePath: image.path})
+		})) image: Express.Multer.File,
+		@Res() res: Response
+	): Promise<Observable<Object> > {
+		if (! await this.userService.saveUserAvatarPath(id, image.path))
+			// return  res.status(HttpStatus.NOT_FOUND).send('jfj')
+		return of({
+			imagePath: image.path,
+			message: 'the avatar uploaded succesfuly'
+		})
 	}
-    @Get()
-    @UseGuards(JwtGuard)
-    async getUserData(@Req() req: Request)
-    {
-        const user = await this.userService.getUserFromJwt(req.cookies['access_token']);
-        const userData = {
-            id: user.id,
-            firstname: user.firstname,
-            lastname: user.lastname,
-            username: user.username,
-        };
-        return userData;
-    }
-    @Delete('delete/:id')
-    async deleteUser(@Param('id') userId: number) // return success
-    {
-        await this.userService.deleteUserFromDB(userId);
-        return 'success';
-    }
 
-    @Get('achievements/:id')
-    async getAchievements(@Param('id') id: number) {
-        return await this.userService.getAchievement(id);
-    }
+	@Put()
+	async updateAvatar() {
 
-    @Get('leaders')
-    async getGameLeaders() {
-        return this.userService.getLeaderBoard()
-    }
-
-    @Get(':id')
-    async getUserById(@Param('id', ParseIntPipe) id: number) {
-        return await this.userService.findUserById(id)
-    }
-
-    @Get('block/:userId')
-    async getBlockedUser(
-        @Param('userId', ParseIntPipe) userId: number,
-        @Req() req: Request
-    )
-    {
-        const user = await this.userService.findUserByEmail(req.user["email"])
-        const User1 =  await this.userService.getBlockedUsers(user.id)
-        return await this.userService.getBlockedUsers(userId)
-    }
-    @Post('block/:userId')
-    async blockUser(
-        @Param('userId', ParseIntPipe) userId: number,
-        @Req() req: Request,
-        @Res() res: Response
-    ) {
-        // console.log(req.user['email'], "ok")
-        const user = await this.userService.findUserByEmail(req.user['email'])
-        console.log(user.id, userId)
-        await this.userService.blockUser(userId, user)
-        return res.status(HttpStatus.OK).send('the user blocked ')
-    }
-
-    @Get()
-    async getUserFromJwt(@Req() req: Request)
-    {
-        const user = await this.userService.getUserFromJwt(req.cookies['access_token'] || req.headers.authorization)
-        if (!user)
-            throw new UnauthorizedException()
-        return {
-            id: user.id,
-            username: user.username,
-        }
-    }
+	}
 
 
-   
-    @Get('image/:id')
-    @Header('Content-Type', 'image/png')
-    async getPictureById(@Param('id', ParseIntPipe) id: number) : Promise<StreamableFile>
-    {
-        const filename = id + '.png';
-        const imagePath = path.join(process.cwd(), 'src/usersImage', filename);
-        const fileContent = createReadStream(imagePath)
-        return new StreamableFile(fileContent);
-    }
-    @Get('stats/:userId')
-    async getStatsById( @Param('userId', ParseIntPipe) id: number) {
-       return await this.userService.getStatsById(id)
-    }
+	@Get()
+	@UseGuards(JwtGuard)
+	async getUserData(@Req() req: Request) {
+		const user = await this.userService.getUserFromJwt(req.cookies['access_token']);
+		const userData = {
+			id: user.id,
+			firstname: user.firstname,
+			lastname: user.lastname,
+			username: user.username,
+		};
+		return userData;
+	}
+	@Delete('delete/:id')
+	async deleteUser(@Param('id') userId: number) // return success
+	{
+		await this.userService.deleteUserFromDB(userId);
+		return 'success';
+	}
 
-    @Get('achievement/firstThree/:id')
-    async getLastThree(@Param('id') id: number)
-    {
-        return await this.userService.getLastThreeAchievements(id);
-    }
-    @Get('achievement/image/:id')
-    @Header('Content-Type', 'image/jpg')
-    async getAchievementImage(@Param('id', ParseIntPipe) id: number) // todo add parseInt pipe
-    {
-        const filename = id + '.jpg';
-        const imagePath = path.join(process.cwd(), 'src/achievementImages', filename);
-        const fileContent = createReadStream(imagePath);
-        return new StreamableFile(fileContent);
-    }
-    @Get('onlinefriends/:id')
-    async getOnlineFriends(@Param('id') id: number)
-    {
-        return await this.userService.onlineFriends(id);
-    }
-    @Post('addfriend/:id')
-    async addFriend(@Param('id') id: number, @Query('friendId') friendId: number, @Res() res: Response)
-    {
-        await this.userService.addFriend(id, friendId);
-        return res.status(201).send(`user has ${friendId} as a friend now`);
-    }
-    @Get('allfriends/:id')
-    async getAllFriends(@Param('id') id: number)
-    {
-        return await this.userService.AllFriends(id);
-    }
-    // @Get('generate2fa/:id')
-    // @UseGuards(JwtGuard)
-    // generate2faForUser(@Param('id') id: number, @Req() req: Request)
-    // {
-    //     const data = await this.userService.generate2fa(id);
+	@Get('achievements/:id')
+	async getAchievements(@Param('id') id: number) {
+		return await this.userService.getAchievement(id);
+	}
 
-    @Get('game/history/:userId')
-    async getGameHistory(@Param('userId', ParseIntPipe) userId: number) : Promise<Match_history[]> {
-        return await this.userService.getMatchHistory(userId)
-    }
+	@Get('leaders')
+	async getGameLeaders() {
+		return this.userService.getLeaderBoard()
+	}
 
-    @Get('2fa/turn-on/:id')
-    @UseGuards(JwtGuard)
-    async turnOn2fa(@Param('id') id: number, @Req() req: Request, @Res() res: Response)
-    {
-        const user = await this.userService.findUserById(id);
-        const data2fa = this.userService.otpsetup(user);
-        user.two_factor_secret = data2fa.secret;
-        user.otpPathUrl = data2fa.otpPathUrl;
-        user.is_two_factor = true;
-        await this.userService.saveUser(user);
-        return res.status(200).send('two factor was turned on')
-    }
+	@Get(':id')
+	async getUserById(@Param('id', ParseIntPipe) id: number) {
+		return await this.userService.findUserById(id)
+	}
 
-    @Get('2fa/turn-off/:id')
-    @UseGuards(JwtGuard)
-    async turnOff2fa(@Param('id') id: number, @Req() req: Request, @Res() res: Response)
-    {
-        const user = await this.userService.findUserById(id);
-        user.two_factor_secret = null;
-        user.otpPathUrl = null;
-        user.is_two_factor = false;
-        await this.userService.saveUser(user);
-        return res.status(200).send('two factor was turned off')
-    }
+	@Get('block/:userId')
+	async getBlockedUser(
+		@Param('userId', ParseIntPipe) userId: number,
+		@Req() req: Request
+	) {
+		const user = await this.userService.findUserByEmail(req.user["email"])
+		const User1 = await this.userService.getBlockedUsers(user.id)
+		return await this.userService.getBlockedUsers(userId)
+	}
+	@Post('block/:userId')
+	async blockUser(
+		@Param('userId', ParseIntPipe) userId: number,
+		@Req() req: Request,
+		@Res() res: Response
+	) {
+		// console.log(req.user['email'], "ok")
+		const user = await this.userService.findUserByEmail(req.user['email'])
+		console.log(user.id, userId)
+		await this.userService.blockUser(userId, user)
+		return res.status(HttpStatus.OK).send('the user blocked ')
+	}
 
-    @Post('2fa/login/')
-    @UseGuards(JwtGuard)
-    async login2fa(@Req() req: Request, @Res() res: Response)
-    {
-        const user = await this.userService.getUserFromJwt(req.cookies['access_token'])
-        const isCodeValid = this.userService.isUserAuthValid(
-            req.body.token,
-            user
-        );
-        console.log(isCodeValid);
-        if(!isCodeValid)
-            return res.status(400).send('two factor token is invalid');
-        return res.status(200).send('correct two factor token');
-    }
-
-    @Get('logout/:id')
-    @UseGuards(JwtGuard)
-    async logout(@Param('id') id: number, @Req() req: Request, @Headers('Authorization') authtoken: string)
-    {
-        const user = await this.userService.findUserById(id);
-        const payload = this.jwt.verify(authtoken.split(' ')[1], {secret: process.env.JWT_SECRET});
-        const till = payload.iat + 86400;
-        await this.BlockedTokenService.blacklistToken(authtoken.split[1](), till * 1000)
-    }
-
-    @Patch('stat/add')
-    async addUserStat (@Query() statDto: StatsDto, @Req() req: Request) {
-       await this.userService.addUserStat(statDto, req.user)
-    }
-
-    @Post('gameHistory/add')
-    @HttpCode(HttpStatus.CREATED)
-    async createGameHistory(@Body() gameHistoryDto: GameHistoryDto) {
-        console.log(gameHistoryDto)
-        await this.userService.addGameHistory(gameHistoryDto)
-        return {
-            Message: "The content is created"
-        }
-    }
+	@Get()
+	async getUserFromJwt(@Req() req: Request) {
+		const user = await this.userService.getUserFromJwt(req.cookies['access_token'] || req.headers.authorization)
+		if (!user)
+			throw new UnauthorizedException()
+		return {
+			id: user.id,
+			username: user.username,
+		}
+	}
 
 
-    @Get()
-    async   searchForUser(@Query() dto: searchDto) {
-        const {username} = dto
-        console.log(username)
-        return this.userService.searchUser(username)
-        console.log(username);
-    }
+
+	@Get('avatar/:id')
+	@Header('Content-Type', 'image/png')
+	async getAvatarById(@Param('id', ParseIntPipe) id: number): Promise<StreamableFile> {
+		const user = await this.userService.findUserById(id)
+
+		if (!user)
+			throw new HttpException('User Not Found !!', HttpStatus.NOT_FOUND)
+		const imagePath = user.avatar
+		try {
+			await access(imagePath, fsPromises.constants.R_OK)
+			const fileContent = createReadStream(imagePath)
+			return new StreamableFile(fileContent);
+		}
+		catch (e) {
+			throw new NotFoundException('File not found or cannot be read.')
+		}
+
+
+	}
+	@Get('stats/:userId')
+	async getStatsById(@Param('userId', ParseIntPipe) id: number) {
+		return await this.userService.getStatsById(id)
+	}
+
+	@Get('achievement/firstThree/:id')
+	async getLastThree(@Param('id') id: number) {
+		return await this.userService.getLastThreeAchievements(id);
+	}
+	@Get('achievement/image/:id')
+	@Header('Content-Type', 'image/jpg')
+	async getAchievementImage(@Param('id', ParseIntPipe) id: number) // todo add parseInt pipe
+	{
+		const filename = id + '.jpg';
+		const imagePath = path.join(process.cwd(), 'src/achievementImages', filename);
+		const fileContent = createReadStream(imagePath);
+		return new StreamableFile(fileContent);
+	}
+	@Get('onlinefriends/:id')
+	async getOnlineFriends(@Param('id') id: number) {
+		return await this.userService.onlineFriends(id);
+	}
+	@Post('addfriend/:id')
+	async addFriend(@Param('id') id: number, @Query('friendId') friendId: number, @Res() res: Response) {
+		await this.userService.addFriend(id, friendId);
+		return res.status(201).send(`user has ${friendId} as a friend now`);
+	}
+	@Get('allfriends/:id')
+	async getAllFriends(@Param('id') id: number) {
+		return await this.userService.AllFriends(id);
+	}
+	// @Get('generate2fa/:id')
+	// @UseGuards(JwtGuard)
+	// generate2faForUser(@Param('id') id: number, @Req() req: Request)
+	// {
+	//     const data = await this.userService.generate2fa(id);
+
+	@Get('game/history/:userId')
+	async getGameHistory(@Param('userId', ParseIntPipe) userId: number): Promise<Match_history[]> {
+		return await this.userService.getMatchHistory(userId)
+	}
+
+	@Get('2fa/turn-on/:id')
+	@UseGuards(JwtGuard)
+	async turnOn2fa(@Param('id') id: number, @Req() req: Request, @Res() res: Response) {
+		const user = await this.userService.findUserById(id);
+		const data2fa = this.userService.otpsetup(user);
+		user.two_factor_secret = data2fa.secret;
+		user.otpPathUrl = data2fa.otpPathUrl;
+		user.is_two_factor = true;
+		await this.userService.saveUser(user);
+		return res.status(200).send('two factor was turned on')
+	}
+
+	@Get('2fa/turn-off/:id')
+	@UseGuards(JwtGuard)
+	async turnOff2fa(@Param('id') id: number, @Req() req: Request, @Res() res: Response) {
+		const user = await this.userService.findUserById(id);
+		user.two_factor_secret = null;
+		user.otpPathUrl = null;
+		user.is_two_factor = false;
+		await this.userService.saveUser(user);
+		return res.status(200).send('two factor was turned off')
+	}
+
+	@Post('2fa/login/')
+	@UseGuards(JwtGuard)
+	async login2fa(@Req() req: Request, @Res() res: Response) {
+		const user = await this.userService.getUserFromJwt(req.cookies['access_token'])
+		const isCodeValid = this.userService.isUserAuthValid(
+			req.body.token,
+			user
+		);
+		console.log(isCodeValid);
+		if (!isCodeValid)
+			return res.status(400).send('two factor token is invalid');
+		return res.status(200).send('correct two factor token');
+	}
+
+	@Get('logout/:id')
+	@UseGuards(JwtGuard)
+	async logout(@Param('id') id: number, @Req() req: Request, @Headers('Authorization') authtoken: string) {
+		const user = await this.userService.findUserById(id);
+		const payload = this.jwt.verify(authtoken.split(' ')[1], { secret: process.env.JWT_SECRET });
+		const till = payload.iat + 86400;
+		await this.BlockedTokenService.blacklistToken(authtoken.split[1](), till * 1000)
+	}
+
+	@Patch('stat/add')
+	async addUserStat(@Query() statDto: StatsDto, @Req() req: Request) {
+		await this.userService.addUserStat(statDto, req.user)
+	}
+
+	@Post('gameHistory/add')
+	@HttpCode(HttpStatus.CREATED)
+	async createGameHistory(@Body() gameHistoryDto: GameHistoryDto) {
+		console.log(gameHistoryDto)
+		await this.userService.addGameHistory(gameHistoryDto)
+		return {
+			Message: "The content is created"
+		}
+	}
+
+
+	@Get()
+	async searchForUser(@Query() dto: searchDto) {
+		const { username } = dto
+		console.log(username)
+		return this.userService.searchUser(username)
+		console.log(username);
+	}
 }
--- app/backend/src/user/user.service.ts
+++ app/backend/src/user/user.service.ts
@@ -1,11 +1,12 @@
-import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
-import {User} from 'src/databases/user.entity';
-import { ILike, Like, Repository} from 'typeorm';
-import {InjectRepository} from '@nestjs/typeorm'
-import {JwtService} from '@nestjs/jwt';
-import {Achievement} from "../databases/achievement/achievement.entity";
-import {Stats} from "../databases/stats.entity";
-import {Match_history} from "../databases/match_history.entity";
+
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { User } from 'src/databases/user.entity';
+import { ILike, Like, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm'
+import { JwtService } from '@nestjs/jwt';
+import { Achievement } from "../databases/achievement/achievement.entity";
+import { Stats } from "../databases/stats.entity";
+import { Match_history } from "../databases/match_history.entity";
 import { authenticator } from 'otplib';
 import { StatsDto } from './dto/stats-dto';
 import { GameHistoryDto } from './game-history-dto/game-history-dto';
@@ -28,9 +29,17 @@ export class UserService {
     ) {
     }
 
+    async saveUserAvatarPath(userId: number, pathAvatar: string) {
+        const user = await this.userRepo.findOneBy({ id: userId })
+        if (!user)
+            return null
+        user.avatar = pathAvatar
+        await this.userRepo.save(user)
+    }
+
     async blockUser(userId: number, user: User) {
         const blockedUser = await this.userRepo.findOne({
-            where: {id: userId}
+            where: { id: userId }
         })
         if (!blockedUser)
             throw new HttpException('user not found', HttpStatus.NOT_FOUND)
@@ -49,7 +58,7 @@ export class UserService {
     }
 
     async findUserByEmail(email: string): Promise<User> {
-        return await this.userRepo.findOneBy({email: email});
+        return await this.userRepo.findOneBy({ email: email });
     }
 
     async getBlockedUsers(userId: number) {
@@ -65,22 +74,21 @@ export class UserService {
             relations: {
                 blocked_users: true,
             },
-            where : {
+            where: {
                 id: userId,
             }
         })
     }
 
     async findUserById(id: number): Promise<User> {
-        return await this.userRepo.findOneBy({id: id});
+        return await this.userRepo.findOneBy({ id: id });
     }
 
-    async userHasAuth(email: string)
-    {
+    async userHasAuth(email: string) {
         const user = await this.userRepo.findOne({
-            where: {email: email}
+            where: { email: email }
         });
-        if(user.is_two_factor === true)
+        if (user.is_two_factor === true)
             return user;
         return null;
     }
@@ -88,7 +96,7 @@ export class UserService {
         if (!userToken)
             return null;
         const payload = this.jwtService.decode(userToken) as tokenPayload;
-        return await this.userRepo.findOneBy({id: payload.id});
+        return await this.userRepo.findOneBy({ id: payload.id });
     }
 
     decodeJwtCode(userToken: string) {
@@ -118,7 +126,7 @@ export class UserService {
     }
 
     async deleteUserFromDB(id: number): Promise<void> {
-        const user: User = await this.userRepo.findOneBy({id: id});
+        const user: User = await this.userRepo.findOneBy({ id: id });
         await this.userRepo.remove(user);
     }
 
@@ -134,7 +142,7 @@ export class UserService {
             where: {
                 id: id
             },
-            relations: {stat: true}
+            relations: { stat: true }
         })
     }
 
@@ -175,7 +183,7 @@ export class UserService {
 
     async getAchievement(id: number) {
         const user = await this.userRepo.findOne({
-            where: {id: id},
+            where: { id: id },
             relations: {
                 stat: {
                     achievements: true,
@@ -187,14 +195,13 @@ export class UserService {
 
     async getLastThreeAchievements(id: number) {
         const achieved = await this.achieveRepo.find({
-            where: {is_achieved: true, user_id: id}
+            where: { is_achieved: true, user_id: id }
         })
         return achieved.slice(0, 3);
     }
-    async onlineFriends(id: number)
-    {
+    async onlineFriends(id: number) {
         const user = await this.userRepo.findOne({
-            where: {id: id},
+            where: { id: id },
             relations: {
                 friends: {
                     stat: true,
@@ -205,10 +212,9 @@ export class UserService {
         return friends;
 
     }
-    async AllFriends(id: number)
-    {
+    async AllFriends(id: number) {
         const user = await this.userRepo.findOne({
-            where: {id: id},
+            where: { id: id },
             relations: {
                 friends: {
                     stat: true,
@@ -217,22 +223,20 @@ export class UserService {
         });
         return user;
     }
-    async addFriend(userId: number, friendId: number)
-    {
+    async addFriend(userId: number, friendId: number) {
         const user = await this.userRepo.findOne({
-            where: {id: userId},
+            where: { id: userId },
             relations: {
                 friends: true,
             }
         });
         const friend = await this.userRepo.findOne({
-            where: {id: friendId},
+            where: { id: friendId },
         });
         user.friends.push(friend);
         await this.userRepo.save(user);
     }
-    async generate2fa(user: User)
-    {
+    async generate2fa(user: User) {
         const secret = authenticator.generateSecret();
         const otpPathUrl = authenticator.keyuri(user.email, 'Transcendence', secret);
         user.two_factor_secret = secret;
@@ -244,8 +248,7 @@ export class UserService {
         }
     }
 
-    otpsetup(user: User)
-    {
+    otpsetup(user: User) {
         const secret = authenticator.generateSecret();
         const otpPathUrl = authenticator.keyuri(user.email, 'Transcendence', secret);
         return {
@@ -254,8 +257,7 @@ export class UserService {
         }
     }
 
-    isUserAuthValid(access_token: string, user: User)
-    {
+    isUserAuthValid(access_token: string, user: User) {
         console.log(access_token, user.two_factor_secret);
         return authenticator.verify({
             token: access_token,
@@ -266,42 +268,42 @@ export class UserService {
 
     async addUserStat(statDto: StatsDto, userReq: any) {
 
-		const user = await this.userRepo.findOne({
-			where: {
-				email : userReq['email']
-			},
-			relations: {
-				stat: true
-			},
-			select: {
-				id: true,
-			}
-		})
+        const user = await this.userRepo.findOne({
+            where: {
+                email: userReq['email']
+            },
+            relations: {
+                stat: true
+            },
+            select: {
+                id: true,
+            }
+        })
 
-		console.log(user)
-		if (!user || !user.stat)
-			throw new HttpException('User Not found Or failed to create stat' , HttpStatus.NOT_FOUND)
+        console.log(user)
+        if (!user || !user.stat)
+            throw new HttpException('User Not found Or failed to create stat', HttpStatus.NOT_FOUND)
 
 
-		user.stat.losses = user.stat.losses + statDto.losses
-		user.stat.wins = user.stat.wins + statDto.wins
-		user.stat.ladder_level += statDto.ladder_level
-		user.stat.xp += statDto.xp
-		await this.statsRepo.save(user.stat)
+        user.stat.losses = user.stat.losses + statDto.losses
+        user.stat.wins = user.stat.wins + statDto.wins
+        user.stat.ladder_level += statDto.ladder_level
+        user.stat.xp += statDto.xp
+        await this.statsRepo.save(user.stat)
     }
 
 
 
-    async   addGameHistory(gameHistoryDto: GameHistoryDto) {
+    async addGameHistory(gameHistoryDto: GameHistoryDto) {
 
         let match_history = new Match_history()
         match_history.opponent_score = gameHistoryDto.opponent_score
         match_history.user_score = gameHistoryDto.user_score
 
-		try {
-        	match_history.opponent = await this.findUserById(gameHistoryDto.opponentId)
+        try {
+            match_history.opponent = await this.findUserById(gameHistoryDto.opponentId)
             console.log(match_history.opponent)
-			let user = await this.findUserById(gameHistoryDto.userId)
+            let user = await this.findUserById(gameHistoryDto.userId)
             console.log(user)
             const history = await this.userRepo.find({
                 relations: {
@@ -314,12 +316,12 @@ export class UserService {
             if (!history['match_history'])
                 user.match_history = [match_history]
             else
-			    user.match_history = [...user.match_history, match_history]
-			await this.matchHistoryRepo.save(match_history)
-		}
-		catch(e) {
+                user.match_history = [...user.match_history, match_history]
+            await this.matchHistoryRepo.save(match_history)
+        }
+        catch (e) {
             console.log(e)
-			throw new HttpException("The User Not found", HttpStatus.NOT_FOUND)
-		}
-	}
+            throw new HttpException("The User Not found", HttpStatus.NOT_FOUND)
+        }
+    }
 }
