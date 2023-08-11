57987af74b90b4836e104b4fbd4375d24d78d800
OussamaBOUIZ <oussamabouizgarne861@gmail.com>
2023-08-11 20:29:25 +0100

    Refactor: Splitting ChatMainInit to ChatDmInit and ChatRoomsInit

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,19 +1,3 @@
-// import React from "react"
-// import Home from "./components/Home"
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-// import Sign from "./Pages/Sign/Sign"
-
-// export default function App() {
-//     return (
-//         <Router>
-//             <Routes>
-//                 <Route path="/" element={<Sign />} />
-//                 <Route path="/home" element={<Home />} />
-//             </Routes>
-//         </Router>
-//     )
-// } 
-
 import React from 'react'
 import ChatLayout from './Pages/Chat/ChatLayout'
 import Home from './Pages/Home/Home'
@@ -30,29 +14,7 @@ import './scss/utils.scss'
 import Prompt from './Pages/Prompt/Prompt'
 import Profile from './Pages/Profile/Profile'
 import Friends from './Pages/Friends/Friends'
-import ChatMainInit from './Pages/Chat/ChatMainInit'
-
-
-// export default function App () {
-//   return (
-//     <BrowserRouter>
-//     <div>
-//       <Header />
-//       <main>
-//       <Sidebar />
-//       <Routes>
-//         <Route path="/" element={<Home />}/>
-//         <Route path="/chat" element={<Chat />}/>
-//         <Route path="/sign" element={<Sign />} />
-//         <Route path="/game" element={<Game />} />
-//         <Route path="/friends" element={<Friends />} />
-//         <Route path="/settings" element={<Settings />} />
-//       </Routes>
-//       </main>
-//       </div>
-//     </BrowserRouter>
-//   );
-// }
+import ChatDmInit from './Pages/Chat/ChatDmInit'
 
 export default function App () {
   return (
@@ -63,7 +25,7 @@ export default function App () {
           <Route element={<MainLayout />}>
             <Route path="/" element={<Home />}/>
             <Route path="/chat" element={<ChatLayout />}>
-                <Route path="init" element={<ChatMainInit /> }/>
+                <Route path="init" element={<ChatDmInit /> }/>
             </Route>
             <Route path="/game" element={<Game />} />
             <Route path="/friends" element={<Friends />} />
