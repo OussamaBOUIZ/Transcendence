2c352ce889cfd960ccda68ec0bf4d164f1a32771
OussamaBOUIZ <oussamabouizgarne861@gmail.com>
2023-08-25 19:28:22 +0100

    Purifying the messageoverview link

--- app/client/src/Pages/Chat/MessageOverview.tsx
+++ app/client/src/Pages/Chat/MessageOverview.tsx
@@ -2,21 +2,20 @@ import React from 'react'
 import {NavLink} from 'react-router-dom'
 
 export default function MessageOverview ({id} : {id:number}) {
-    const [activeStyle, setActiveStyle] = React.useState<boolean>(false);
 
     return (
         <NavLink 
-        to={`/chat/${id}`}
-        style={(isActive: boolean) => setActiveStyle(isActive)}
+        to={`/chat/${id}`} 
+        className={`message_oview 
+                 ${(isActive: boolean) => isActive ? 'active' : ''}`}
         >
-        <figure className={`message_oview ${activeStyle ? "active": ""}`}>
             <img src="../src/Assets/cat.jpg" alt="Cat pic" />
             <figcaption>
                 <h4>Elegant</h4>
                 <p>Nothing received</p>
             </figcaption>
             <time>Yesterday</time>
-        </figure>
         </NavLink>
     );
 }
+
