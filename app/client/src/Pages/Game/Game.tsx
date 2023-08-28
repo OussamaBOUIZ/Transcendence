14370592b87c7f49a1cfd62692500a8f3293b13c
YassineEddyb <yassineeddyb@gmail.com>
2023-08-28 12:17:46 +0100

    In Progress: basic game frontend

--- app/client/src/Pages/Game/Game.tsx
+++ app/client/src/Pages/Game/Game.tsx
@@ -2,16 +2,11 @@ import React, {useState} from 'react'
 import { ReactP5Wrapper } from "react-p5-wrapper"
 import sketch from "./Skitch"
 
-// const gameStyles = {
-//     display: "grid",
-//     placeItems: "center"
-// }
-
 
 export default function Game () {
     const [rotation, setRotation] = useState(6);
 
     return (
-        <ReactP5Wrapper sketch={sketch} rotation={rotation}/>
+        <ReactP5Wrapper sketch={sketch} theme="black"/>
     );
 }
--- app/client/src/Pages/Game/Skitch.ts
+++ app/client/src/Pages/Game/Skitch.ts
@@ -2,22 +2,24 @@ import { SketchProps, P5CanvasInstance } from "react-p5-wrapper";
 
 interface MySketchProps extends SketchProps {
     rotation: number;
+    theme: string;
 }
 
 interface Velocity {
     x: number;
-    y: number
+    y: number;
 }
 
-const PH: number = 300;
+const PH: number = 80;
 const PW: number = 10;
 const GAP: number = 10;
 const PSPEED: number = 10;
 const RADIUS: number = 10;
+const SPEED: number = 6;
 
 const vel: Velocity = {
-    x: 5,
-    y: 5,
+    x: 0,
+    y: 0,
 };
 
 function collision(pad: Pad, ball: Ball): boolean {
@@ -48,7 +50,6 @@ class Ball {
     y: number;
     r: number;
 
-
     constructor(x: number, y:number, r:number) {
         this.x = x;
         this.y = y;
@@ -64,12 +65,11 @@ class Ball {
         this.x += vel.x;
         this.y += vel.y;
         
-        if (this.y < 0 || this.y > p5.height)
+        if (this.y < (this.r * 2) || this.y > p5.height - (this.r * 2))
             vel.y *= -1;
 
         if (this.x < 0 || this.x > p5.width) {
-            this.x = p5.width / 2;
-            this.y = p5.height / 2;
+            reset(p5);
         }
     }
 }
@@ -94,16 +94,21 @@ class Pad {
     updatePad(p5: any, ball: Ball) {
         this.drawPad(p5);
         
-        if (p5.keyIsDown(p5.UP_ARROW) && this.y > GAP) {
+        if (p5.keyIsDown(p5.UP_ARROW) && this.y > GAP)
             this.y -= PSPEED;
-        }
         
-        if (p5.keyIsDown(p5.DOWN_ARROW) && this.y + this.h < p5.height - GAP) {
+        if (p5.keyIsDown(p5.DOWN_ARROW) && this.y + this.h < p5.height - GAP)
             this.y += PSPEED;
-        }
 
-        if (collision(this, ball))
-            vel.x *= -1;
+        if (collision(this, ball)) {
+            const diff: number = (ball.y - (this.y - this.h / 2)) / (this.h / 2);
+            const angle: number = (p5.PI / 8) * diff;
+
+            let dir: number = ball.x > p5.width / 2 ? -1 : 1;
+
+            vel.x = SPEED * dir * p5.cos(angle);
+            vel.y = SPEED * dir * p5.sin(angle);
+        }
     }
 }
 
@@ -111,26 +116,42 @@ let leftPad: Pad;
 let rightPad: Pad;
 let ball: Ball;
 
+function reset(p5: any): void {
+    const angle: number = p5.random(p5.PI / 4, -p5.PI / 4);
+    vel.x = SPEED * p5.cos(angle);
+    vel.y = SPEED * p5.sin(angle);
+
+    vel.x *= p5.random(1) < 0.5 ? -1 : 1;        
+
+    leftPad = new Pad(GAP, (p5.height / 2) - PH / 2, PW, PH);
+    rightPad = new Pad(p5.width - PW - GAP, (p5.height / 2) - PH / 2, PW, PH);
+    ball = new Ball(p5.width / 2, p5.height / 2, RADIUS);
+}
+
 function sketch(p5: P5CanvasInstance<MySketchProps>) {
-    let state = {
+    let props: MySketchProps = {
         rotation: 0,
+        theme: "",
     }
 
-    p5.updateWithProps = (props: any) => {
-        state = Object.assign(state, props)
+    p5.updateWithProps = (p: any) => {
+        props = Object.assign(props, p)
     };
 
     p5.setup = (): void => {
-        p5.createCanvas(800, 500);
-        leftPad = new Pad(GAP, (p5.height / 2) - PH / 2, PW, PH);
-        rightPad = new Pad(p5.width - PW - GAP, (p5.height / 2) - PH / 2, PW, PH);
-        ball = new Ball(p5.width / 2, p5.height/2, RADIUS);
+        p5.createCanvas(600, 400);
+        reset(p5);
     }
 
-    console.log(state.rotation);
 
-    p5.draw = (props: any): void => {
-        p5.background(150)
+    p5.draw = (): void => {
+        if (props.theme === "black")
+            p5.background(0);
+        else if (props.theme == "white")
+            p5.background(255);
+        else if (props.theme === "grey")
+            p5.background(200);
+
         leftPad.updatePad(p5, ball);
         rightPad.updatePad(p5, ball);
         ball.updateBall(p5);
