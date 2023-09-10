9ff50a286d3d1e21c3bb29543b007a74ad4020c1
Oussama Ouazize <oouazize@e1r3p4.1337.ma>
2023-09-10 15:00:52 +0100

    change the profile image of game

--- app/client/src/Pages/Game/Game.tsx
+++ app/client/src/Pages/Game/Game.tsx
@@ -6,20 +6,19 @@ import { NavLink } from 'react-router-dom';
 import img from "../../Assets/BrightGround.jpeg"
 import img2 from "../../Assets/DreamShaper_32_young_man_character_playing_ping_pong_full_body_2.jpeg"
 
-
 export default function Game() {
 
     return (
         <section className="flex flex-col justify-center items-center w-full h-full">
-            <div className='usersBG absolute flex w-full h-full top-0'>
+            {/* <div className='usersBG absolute flex w-full h-full top-0'>
                 <div className='users leftUser'><img src={img} alt="" /></div>
                 <div className='users rightUser'><img src={img2} alt="" /></div>
-            </div>
+            </div> */}
             <NavLink to={'/'} className="logout absolute cursor-pointer z-50">
                 <FaSignOutAlt />
             </NavLink>
             <div className='bg absolute w-full h-full top-0'></div>
-            <div className='main-container flex flex-col justify-center'>
+            <div className='main-container flex flex-col justify-center gap-1'>
                 <Board />
                 <div className='table z-50'></div>
             </div>
