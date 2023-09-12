bd6ef374499a5097fa76028ba18c0222f924d772
OussamaBOUIZ <oussamabouizgarne861@gmail.com>
2023-09-12 21:11:59 +0100

    Alternating carousel with simple game cards

--- app/client/src/Components/GameCarousel.tsx
+++ app/client/src/Components/GameCarousel.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import Swiper from 'swiper';
-import {Navigation,  Pagination } from 'swiper/modules'
+import {Link} from 'react-router-dom';
 
-var swiper = new Swiper(".swiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 2,
-    speed: 600,
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 3,
-      slideShadows: true,
-    },
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: false,
-    },
-  });
+const data = [
+  {
+    "name": "Battle Royal",
+    "image": "/src/Assets/GameArea/galaxy.jpg",
+    "description": "Play through the story alone against AI opponents",
+  },
+  {
+    "name": "Blazing Pong", 
+    "image": "/src/Assets/GameArea/snowy-mount.jpg",
+    "description": "Team up with friends to complete missions together",
+  },
+  {
+    "name": "Arctic Pong",
+    "image": "/src/Assets/GameArea/arcticarea.jpg", 
+    "description": "Compete head-to-head against other players",
+  },
+  {
+     "name": "Retro Pong",
+     "image": "/src/Assets/GameArea/RetroPong.jpg",
+     "description": "See how long you can last against endless waves of enemies",  
+  }
+]
+
+function ModeCard({name, image}) {
+  return (
+    <Link>
+      <figure style={{backgroundImage: `url(${image})`}}
+        className="rounded-xl flex justify-center items-end w-96 h-64 p-10 bg-no-repeat bg-cover bg-center"
+      >
+          <h2>{name}</h2>
+      </figure>
+    </Link>
+  )
+}
 
 export default function GameCarousel () {
     return (
@@ -28,11 +42,16 @@ export default function GameCarousel () {
         <header className='p-6 bg-slate-500'>
             <button name='back' className='text-xl font-semibold hover:underline'> Go Back</button>
         </header>
-        <div>
-            
-
-        </div>
-
+        <section className='game-modes-container h-5/6 px-20'>
+          <div className="flex justify-between my-5">
+            <ModeCard name="Battle Royal" image="/src/Assets/GameArea/galaxy.jpg"/>      
+            <ModeCard name="Blazing Pong" image="/src/Assets/GameArea/snowy-mount.jpg"/>      
+          </div>
+          <div className="flex justify-between my-5">
+            <ModeCard name="Arctic Pong" image="/src/Assets/GameArea/arcticarea.jpg"/>      
+            <ModeCard name="Retro Pong" image="/src/Assets/GameArea/RetroPong.jpg"/>      
+          </div>
+        </section>
     </div>
     );
 }
