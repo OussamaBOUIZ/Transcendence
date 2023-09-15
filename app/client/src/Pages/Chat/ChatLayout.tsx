72ac7801089cb08346375cb62ddb43ace7b13d77
Oussama Ouazize <oouazize@c3r7p7.1337.ma>
2023-09-15 19:00:48 +0100

    fix styling of chat nav

--- app/client/src/Pages/Chat/ChatLayout.tsx
+++ app/client/src/Pages/Chat/ChatLayout.tsx
@@ -3,24 +3,25 @@ import React, { useContext } from 'react'
 import ChatAccount from './ChatAccount';
 import "../../scss/chat.scss"
 import OnlineNow from './OnlineNow';
-import {Outlet, NavLink} from 'react-router-dom'
+import {Outlet, NavLink, useLocation} from 'react-router-dom'
 import InboxContext, { InboxProvider } from '../../Context/InboxContext';
 import UserContext from '../../Context/UserContext';
 
 function Navi () {
     const {inboxList} = useContext(InboxContext);
     const counter = inboxList.current.reduce((accum, current) => accum += current.unseenMessages ? current.unseenMessages : 0, 0)
+    const chatRegex = /^\/chat(\/)?(\d+)?$/;
+    const {pathname} = useLocation();
+    
     return (
         <nav className='flex justify-between'>
-            <NavLink  
-            className={`nav_link flex justify-center items-center ${(isActive:boolean) => isActive ? 'active' : ''} flex`} 
-            to="/chat" end>
+            <NavLink to="/chat"
+            className={`nav_link flex justify-center items-center ${chatRegex.test(pathname) ? 'active' : 'disactive'}`}>
                 Messages
                 {counter !== 0 && <span className=' rounded-lg block bg-pink-500 px-2 h-5 text-sm'>{counter}</span>}
             </NavLink>
-            <NavLink 
-            className={`nav_link flex justify-center items-center ${(isActive:boolean) => isActive ? 'active' : ''}`}  
-            to="/chat/rooms">
+            <NavLink to="/chat/rooms"
+            className={`nav_link flex justify-center items-center ${!chatRegex.test(pathname) ? 'active' : 'disactive'}`}>
                 Channels
             </NavLink>
         </nav>
